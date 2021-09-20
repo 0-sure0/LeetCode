8f952f7d4f995f45d8fe5397981b6ac33798f59e
outli2r <80079529+outli2r@users.noreply.github.com>
2021-09-20 16:51:08 +0900

    Update reorder-data-in-log-files.py

--- reorder-data-in-log-files/reorder-data-in-log-files.py
+++ reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -2,6 +2,7 @@ class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letter_logs = []
         digit_logs = []
+        
         for log in logs:
             log = log.split()
             content = log[1:]
@@ -9,10 +10,22 @@ class Solution:
                 digit_logs.append(' '.join(log))
             else:
                 letter_logs.append(log)
-
+       
         letter_logs.sort(key=lambda x: (x[1:], x[0]))
         for i in range(len(letter_logs)):
             letter_logs[i] = ' '.join(letter_logs[i])
 
         return letter_logs + digit_logs
+    
+     '''
+        letter_logs, digit_logs = [], []
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                digit_logs.append(log)
+            else:
+                letter_logs.append(log)
+        letter_logs.sort(key = lambda x: (x.split()[1:], x.split()[0]))
+        return letter_logs + digit_logs
+        '''
             
