1b70ae71747958034bdddabd49d084a0c080dd29
outli2r <80079529+outli2r@users.noreply.github.com>
2021-09-24 09:01:14 +0900

    Update most-common-word.py

--- most-common-word/most-common-word.py
+++ most-common-word/most-common-word.py
@@ -14,7 +14,7 @@ class Solution:
         '''
         words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
         counts = Counter(words)        
-        
+        return counts.most_common(1)[0][0]
         '''
         
         return paragraph.most_common(1)[0][0]
