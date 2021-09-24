720219ae829717c30829c844d9e3be7df17de356
outli2r <80079529+outli2r@users.noreply.github.com>
2021-09-24 09:00:49 +0900

    Update most-common-word.py

--- most-common-word/most-common-word.py
+++ most-common-word/most-common-word.py
@@ -4,13 +4,18 @@ import re
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         paragraph = paragraph.lower()
-        paragraph = re.sub("[^a-z]", " ", paragraph).split()        
-        print('listed: ', paragraph)
+        paragraph = re.sub("[^a-z]", " ", paragraph).split()                
         paragraph = Counter(paragraph)
-        print('paragrph: ' , paragraph)
+        
         for word in banned:
             del paragraph[word]      
 
-        print('deleted: ' ,paragraph)
+            
+        '''
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+        counts = Counter(words)        
+        
+        '''
+        
         return paragraph.most_common(1)[0][0]
         
