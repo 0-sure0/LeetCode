0947675d77b70787903ca46468e960b2b472f056
outli2r <80079529+outli2r@users.noreply.github.com>
2021-12-09 17:24:27 +0900

    Time: 332 ms (51.80%), Space: 16.8 MB (69.97%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
      def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        islands = 0
-        
-        def dfs(row, col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != '1':
-                return
-            grid[row][col] = 0
-            dfs(row + 1, col)
-            dfs(row, col + 1)
-            dfs(row - 1, col)
-            dfs(row, col - 1)
-        
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == '1':
-                    dfs(row, col)
-                    islands += 1
-        
-        return islands
-   
-        
+        m = len(grid)
+        n = len(grid[0])
+
+        def is_island(row, col):
+            dr = [-1, 0, 1, 0]
+            dc = [0, 1, 0, -1]
+
+            for i in range(4):
+                x = col + dc[i]
+                y = row + dr[i]
+                if 0 <= y <= m - 1 and 0 <= x <= n - 1 and grid[y][x] == '1':
+                    grid[y][x] = '0'
+                    is_island(y, x)
+            return
+
+        ans = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    grid[r][c] = '0'
+                    is_island(r, c)
+                    ans += 1
+        return ans
