aa60a9b8dd018a10a97806c628ada6ee832092fa
outli2r <80079529+outli2r@users.noreply.github.com>
2021-12-10 21:23:27 +0900

    Time: 776 ms (5.03%), Space: 14.7 MB (8.68%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -1,21 +1,28 @@
-from itertools import combinations
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        answer = []
-        
-        def dfs(cand_sum, idx, path):
-            if cand_sum < 0:
-                return
-            if cand_sum == 0:
-                answer.append(path)
-                return
+        def bfs(q):
+            while q:
+                t = q.popleft()
+
+                for c in candidates:
+                    new_t = t[:]                
+                    if sum(new_t) + c == target:
+                        new_t.append(c)
+                        new_t = tuple(sorted(new_t))
+                        result[new_t] = 1                       
+                    elif sum(new_t) + c < target:
+                        new_t.append(c)
+                        q.append(new_t)
+                    else:
+                        continue
             
-            for i in range(idx, len(candidates)):
-                dfs(cand_sum - candidates[i], i, path + [candidates[i]])
+        result = {}
+        for c in candidates:
+            if c == target:
+                result[tuple([c])] = 1
+                continue
+            else:
+                q = collections.deque([[c]])
+                bfs(q)
         
-        dfs(target, 0, [])
-        return answer
-            
-            
-        
+        return list(map(list, result.keys()))
