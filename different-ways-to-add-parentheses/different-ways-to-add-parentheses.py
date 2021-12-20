511197991a6ae77d7007cc91207c3f53cfac394f
outli2r <80079529+outli2r@users.noreply.github.com>
2021-12-20 11:21:28 +0900

    Time: 36 ms (63.83%), Space: 14.6 MB (22.83%) - LeetHub

--- different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,24 +1,8 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        def compute(left, right, op):
-            ans = []
-            for l in left:
-                for r in right:
-                    ans.append(eval(str(l) + op + str(r)))
-            
-            return ans
-        
-        if expression.isdigit():
-            print(expression)
-            return [int(expression)]
-        
-        ans = []
-        for idx, v in enumerate(expression):
-            if v in '+-*':
-                left = self.diffWaysToCompute(expression[:idx])
-                right = self.diffWaysToCompute(expression[idx + 1:])
-                
-                ans.extend(compute(left, right, v))
-        return ans
+        return [a+b if c == '+' else a-b if c == '-' else a*b
+            for i, c in enumerate(expression) if c in '+-*'
+            for a in self.diffWaysToCompute(expression[:i])
+            for b in self.diffWaysToCompute(expression[i+1:])] or [int(expression)]
   
         
