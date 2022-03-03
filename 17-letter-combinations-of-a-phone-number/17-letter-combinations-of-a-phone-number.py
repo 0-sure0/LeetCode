dcaf9130264191ea2f9b2f9b69f3c31b9ab34137
outli2r <80079529+outli2r@users.noreply.github.com>
2022-03-03 13:12:45 +0900

    Time: 24 ms (98.63%), Space: 13.9 MB (90.38%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(idx, path):
+            if len(path) == len(digits):
+                result.append(path)
+                return
+            
+            for i in range(idx, len(digits)):
+                for j in numbers[digits[i]]:
+                    dfs(i + 1, path + j)
+        
+        if not digits:
+            return []
+        
+        numbers = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        
+        result = []
+        dfs(0, "")
+        
+        return result
